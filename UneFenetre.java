70833611cd0c21a7b8fb6610b95465d1aaeb8742
RayanSUK <sukkarie.rayan@gmail.com>
2025-10-19 15:59:49 +0200

    ajout d'un 2ème mobile avec son prpore bouton

--- UneFenetre.java
+++ UneFenetre.java
@@ -1,50 +1,64 @@
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 import javax.swing.*;
 
-class UneFenetre extends JFrame 
-{
-    UnMobile sonMobile;
-    private final int LARG=400, HAUT=250;
+class UneFenetre extends JFrame {
 
-    private Thread laTache;        // on garde le thread pour suspend/resume
-    private boolean enPause = false;
+    UnMobile sonMobile1, sonMobile2;
+    private final int LARG = 400, HAUT = 250;
 
+    private Thread laTache1, laTache2;
+    private boolean enPause1 = false, enPause2 = false;
 
-    public UneFenetre()
-    {   //appele le constructurer de la classe Jframe
-        super("le Mobile"); //  titre de la barre  de titre de la fenetre
-        // pas obligatoire. possible de mettre this.add(sonMobile)
+    public UneFenetre() {
+        super("Mobiles x2 (Grid 2x2)");
+
         Container leConteneur = getContentPane();
-        sonMobile = new UnMobile(LARG,HAUT);
-        leConteneur.add(sonMobile);
+        // --- Grille 2 lignes x 2 colonnes ---
+        leConteneur.setLayout(new GridLayout(2, 2));
 
-        //ajout du bouton
-        JButton bouton = new JButton("Start/Stop");
-        leConteneur.add(bouton);
+        // ---- Mobile 1 + bouton 1 ----
+        sonMobile1 = new UnMobile(LARG, HAUT);
+        JButton bouton1 = new JButton("Start/Stop 1");
 
+        // ---- Mobile 2 + bouton 2 ----
+        sonMobile2 = new UnMobile(LARG, HAUT);
+        JButton bouton2 = new JButton("Start/Stop 2");
 
-        laTache = new Thread(sonMobile);
-        laTache.start();
+        // --- Ordre d'ajout en GridLayout: B1 | M1 | B2 | M2 ---
+        leConteneur.add(bouton1);
+        leConteneur.add(sonMobile1);
+        leConteneur.add(bouton2);
+        leConteneur.add(sonMobile2);
 
-        bouton.addActionListener(new ActionListener() {
+        // --- Crée et démarre chaque thread ---
+        laTache1 = new Thread(sonMobile1);
+        laTache2 = new Thread(sonMobile2);
+        laTache1.start();
+        laTache2.start();
+
+        // --- Actions des boutons: suspend/resume par mobile ---
+        bouton1.addActionListener(new ActionListener() {
             @Override
             @SuppressWarnings("deprecation")
             public void actionPerformed(ActionEvent e) {
-                if (!enPause) {
-                    laTache.suspend();
-                    enPause = true;
-                } else {
-                    laTache.resume();
-                    enPause = false;
-                }
+                if (!enPause1) { laTache1.suspend(); enPause1 = true; }
+                else            { laTache1.resume();  enPause1 = false; }
             }
         });
 
+        bouton2.addActionListener(new ActionListener() {
+            @Override
+            @SuppressWarnings("deprecation")
+            public void actionPerformed(ActionEvent e) {
+                if (!enPause2) { laTache2.suspend(); enPause2 = true; }
+                else            { laTache2.resume();  enPause2 = false; }
+            }
+        });
 
-
-        setSize(LARG,HAUT);
+        // fenêtre
+        setSize(LARG, HAUT);
+        setLocationRelativeTo(null);
         setVisible(true);
     }
 }
