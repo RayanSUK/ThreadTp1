f989da712662a335969eedfd9aadd18f0cda7a04
RayanSUK <sukkarie.rayan@gmail.com>
2025-10-19 15:54:59 +0200

    ajout du bouton start stop (penser à changer la version < 19)

--- UneFenetre.java
+++ UneFenetre.java
@@ -1,11 +1,17 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 
 class UneFenetre extends JFrame 
 {
     UnMobile sonMobile;
     private final int LARG=400, HAUT=250;
-    
+
+    private Thread laTache;        // on garde le thread pour suspend/resume
+    private boolean enPause = false;
+
+
     public UneFenetre()
     {   //appele le constructurer de la classe Jframe
         super("le Mobile"); //  titre de la barre  de titre de la fenetre
@@ -13,8 +19,31 @@ class UneFenetre extends JFrame
         Container leConteneur = getContentPane();
         sonMobile = new UnMobile(LARG,HAUT);
         leConteneur.add(sonMobile);
-        Thread laTache = new Thread(sonMobile);
+
+        //ajout du bouton
+        JButton bouton = new JButton("Start/Stop");
+        leConteneur.add(bouton);
+
+
+        laTache = new Thread(sonMobile);
         laTache.start();
+
+        bouton.addActionListener(new ActionListener() {
+            @Override
+            @SuppressWarnings("deprecation")
+            public void actionPerformed(ActionEvent e) {
+                if (!enPause) {
+                    laTache.suspend();
+                    enPause = true;
+                } else {
+                    laTache.resume();
+                    enPause = false;
+                }
+            }
+        });
+
+
+
         setSize(LARG,HAUT);
         setVisible(true);
     }
